1348c03998e32cd22ad5a033e23bec4063c0b80f
achaltelmasre <achaltelmasre@gmail.com>
2023-09-14 16:46:20 +0530

    Button component

--- src/views/Menu/Menu.js
+++ src/views/Menu/Menu.js
@@ -1,18 +1,28 @@
-import "./Menu.css"
-import { Link } from "react-router-dom"
+import "./Menu.css";
 
-export default function Navbar() {
-    const path = window.location.pathname;
-    return (
+import Button from "./../../component/Button/Button";
 
-        <div className="nav">
-            <span>My website</span>
+export default function Menu(){
 
-            <Link to="/" className={path==='/'? 'active':''}>HOME</Link>
-            <Link to="/about" className={path==='/about'? 'active':''}>About</Link>
-            <Link to="/contact" className={path==='/contact'? 'active':''}>Contact</Link>
+    return(
+        <>
+    <div className="menu-bg"> 
+           <h1 className="menu-heading menu-head">Menu</h1>
+           <h3 className="menu-head">Indulge in the exquisite and diverse tastes of </h3>
+           <h3 className="menu-head"> Middle Eastern cuisine with our menu.</h3>
+    </div>
+    <div className="btn-div">
+     <Button name='Main Course  >'/>
+     <Button name='By Weight  >'/>
+     <Button name='Family Size  >'/>
+     <Button name='Sandwiches  >'/>
+     <Button name='Salad & Appetizer  >'/>
+     <Button name='Desserts  >'/>
+     <Button name=' Breakfast  >'/>
+     <Button name='Drinks & Sides   >'/>
 
-        </div>
 
+    </div>
+    </>
     )
 }
