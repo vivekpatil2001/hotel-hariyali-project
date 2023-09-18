e77e4c09d2055cc9f8bf5eb5c0cf863c3d4b033d
rahuljadhav2163 <rgj4002@gmail.com>
2023-09-18 11:49:52 +0530

    added inc dec function

--- src/component/Cateringcards/Cards.js
+++ src/component/Cateringcards/Cards.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useState } from "react";
 import './Cards.css';
 import CateringButton from '../Button/CateringButtons/CateringButton';
 
-export default function Cards(props, name, city, buton) {
+export default function Cards(props, name, city, buton){
+
+    const [count, setCount] = useState(1);
+
+    function IncrementValue() {
+        setCount(count + 1);
+    }
+
+    function DecreseValue() {
+        setCount(count - 1);
+    }
+
 
 
 
@@ -13,11 +25,12 @@ export default function Cards(props, name, city, buton) {
             <h1 className='card-heading'>{props.name}</h1>
 
             <h2 className='card-heading'>{props.city}</h2>
-            <button className='catering-btn-incdec'>+</button>
-            <button type='button' className='button-catering' id='quantity'>{props.button} Content</button>
-            <button className='catering-btn-incdec'>-</button>
+            <button className='catering-btn-incdec' onClick={IncrementValue}>+</button>
+            <button type='button' className='button-catering' id='quantity'>{props.button}{count}</button>
+            <button className='catering-btn-incdec' onClick={DecreseValue}>-</button>
 
 
         </div>
     );
 }
+
